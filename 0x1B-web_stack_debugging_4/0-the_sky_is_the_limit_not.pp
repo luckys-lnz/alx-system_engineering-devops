0c6d86d15f16e40ebe7b33e020917686485b478c
luckys-lnz <lalucky22@gmail.com>
2024-08-22 15:48:59 +0000

    the script to complete the task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,45 +1,13 @@
+# This increases the amount of traffic an Nginx server can handle.
 
-# Puppet manifest to increase max open files limit and optimize Nginx
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-class nginx_optimization {
-
-  # Ensure Nginx package is installed
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Increase the maximum number of open files for the Nginx service
-  file_line { 'increase_nginx_nofile_limit':
-    path    => '/etc/systemd/system/nginx.service.d/limits.conf',
-    line    => 'LimitNOFILE=10000',
-    match   => '^LimitNOFILE=',
-    notify  => Exec['reload-systemd'],
-  }
-
-  # Reload systemd to apply the changes
-  exec { 'reload-systemd':
-    command => 'systemctl daemon-reload',
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    notify  => Service['nginx'],
-  }
-
-  # Ensure Nginx service is running and enabled
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/nginx.conf'],
-  }
-
-  # Manage the Nginx configuration file
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    content => template('/home/ubuntu/pupetlabs/templates/0-the_sky_is_the_limit_not.pp'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    notify  => Service['nginx'],
-  }
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
-
-# Apply the class
-include nginx_optimization
